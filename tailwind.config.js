fa54ea6fc215d34f07957d08370cd051af8a8a98
yoloinfinity55 <yoloinfinity55@gmail.com>
2025-10-01 19:38:47 -0400

    updated to fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-
-// ...existing code...
 module.exports = {
   content: [
-    "./assets/notwatching/hugo_stats.json",
     "./layouts/**/*.html",
-    "./themes/quickstart/layouts/**/*.html",
-    "./content/**/*.md"
+    "./content/**/*.{html,md}",
+    "./themes/**/layouts/**/*.html",
   ],
   theme: {
-    extend: {
-      colors: {
-        'custom-red': '#ff0000'
-      }
-    }
+    extend: {},
   },
-  plugins: []
-};
+  plugins: [],
+}
